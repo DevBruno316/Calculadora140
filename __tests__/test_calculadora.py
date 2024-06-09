8b0a27505be4583acd577d20120898638a8a6185
Bruno <studybruno000@gmail.com>
2024-06-09 20:36:45 -0300

    Criamos as 4 operações aritméticas básicas e testes de unidade simples e data driven

--- __tests__/test_calculadora.py
+++ __tests__/test_calculadora.py
@@ -1,5 +1,5 @@
 import pytest
-
+from utils.utils import read_csv
 from calculadora.calculadora import somar_dois_numeros,subtrair_dois_numeros,multiplicar_dois_numeros,dividir_dois_numeros
 
 
@@ -58,9 +58,53 @@ def test_dividir_dois_numeros():
 def test_dividir_por_zero():
     num1 = 320
     num2 = 0
+    resultado_esperado = 'Erro: Não é possível dividir por 0'
 
     resultado_obtido = dividir_dois_numeros(num1,num2)
 
-    assert resultado_obtido
+    assert resultado_obtido == resultado_esperado
+
+# Testes baseados em dados/ Data Driven Tests (DTT)
+    #Dados em uma lista
+    # Dados em um arquivo, vários formatos: csv (texto separado por vírgulas), json, xml, dat
+
+@pytest.mark.parametrize('num1, num2, resultado_esperado',
+                         [ #array/matriz
+                             (5,7,12),
+                            (0,8,8),
+                             (10,-15,-5),
+                              (6,0.75,6.75) #tupla / registro
+                         ]
+                         )
+
+def test_somar_dois_numeros_lista(num1,num2,resultado_esperado):
+
+    # Padrão/ Standard AAA (se diz Triple A/3A) = Arrange, Act, Assert
+
+    # Arrange / Prepara / Configura
+    #Dados de entrada e saída fornecidos pela massa de teste em formato de lista
+
+    #Act / Executa
+    resultado_obtido = somar_dois_numeros(num1,num2)
+
+    # Assert / Valida
+    assert resultado_esperado == resultado_obtido
+
+@pytest.mark.parametrize('num1,num2,resultado_esperado', 
+                           read_csv('./fixtures/massa_somar.csv')
+                         )
+
+def test_somar_dois_numeros_csv(num1,num2,resultado_esperado):
+
+    # Padrão/ Standard AAA (se diz Triple A/3A) = Arrange, Act, Assert
+
+    # Arrange / Prepara / Configura
+    #Dados de entrada e saída fornecidos pela massa de teste em formato de lista
+
+    #Act / Executa
+    resultado_obtido = somar_dois_numeros(float(num1),float(num2))
+
+    # Assert / Valida
+    assert float(resultado_esperado) == resultado_obtido
 
     
